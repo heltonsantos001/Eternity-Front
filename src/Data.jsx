e563517be6c8a412a7b0dfc8442b08ff87c1c328
heltonsantos001 <heltonsanto001@gmail.com>
2023-07-31 21:01:17 -0300

    configurando o react

---
+++ src/Data.jsx
@@ -0,0 +1,21 @@
+export const post =[
+    {
+        title: "teste1",
+        image: "https://s1.static.brasilescola.uol.com.br/be/conteudo/images/imagem-em-lente-convexa.jpg",
+        likes: 2,
+        comments:3
+    },
+    {
+        title: "teste2",
+        image: "https://s1.static.brasilescola.uol.com.br/be/conteudo/images/imagem-em-lente-convexa.jpg",
+        likes: 2,
+        comments: 3
+    },
+    {
+        title: "teste3",
+        image: "https://s1.static.brasilescola.uol.com.br/be/conteudo/images/imagem-em-lente-convexa.jpg",
+        likes: 2,
+        comments: 3
+    },
+    
+]
